68b37d18bc71bb6d8e612006442132d77fafa983
Slawomir Jurczak <slj@netcompany.com>
2019-01-04 11:32:12 +0100

    add components

--- slavo/src/app/app.module.ts
+++ slavo/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TechnologyComponent } from './technology/technology.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    PortfolioComponent,
+    TechnologyComponent
   ],
   imports: [
     BrowserModule
