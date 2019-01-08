685e8cc93fbe00e09f3c60c70c3c28521f9d69c8
slavoju <github@slavo.pl>
2019-01-08 17:05:31 +0100

    create angular components based on html template

--- slavo/src/app/app.module.ts
+++ slavo/src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { TechnologyComponent } from './technology/technology.component';
+import { HobbyComponent } from './hobby/hobby.component';
+import { SloganComponent } from './slogan/slogan.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,10 @@ import { TechnologyComponent } from './technology/technology.component';
     AboutComponent,
     ContactComponent,
     PortfolioComponent,
-    TechnologyComponent
+    TechnologyComponent,
+    HobbyComponent,
+    SloganComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
