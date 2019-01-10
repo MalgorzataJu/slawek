35fbf6f11f71838bbf1f73201c35def0bf3ba303
Malgorzataju <gmjurczak@gmail.com>
2019-01-10 12:55:41 +0100

    sama :) eksportuję
    Signed-off-by: Malgorzataju <gmjurczak@gmail.com>

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const burger = document.querySelector(".burger");
-
-burger.addEventListener("click", function () {
-    burger.classList.toggle("active")
-    // ico.classList.toggle("active");
+const ico = document.querySelector(".burger ");
+const nav = document.querySelector(".naw");
+ico.addEventListener("click", function () {
+    ico.classList.toggle("active");
+    nav.classList.toggle("show");
 })
